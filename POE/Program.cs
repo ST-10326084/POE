47d30519bc5ec8169290ab5ebad4bccb0df1f3e8
dean <deangibson03@gmail.com>
2024-02-29 10:09:36 +0200

    Add project files.

---
+++ POE/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace POE_PART1 
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // recipe program, where you can track recipes and ingrediants
+            // note (only 1 ) recipe
+            // each step in creating the recipe,
+            // use arrays to store steps in the recipe and ingrediants
+            // must be able to clear the recipe, and add a new one
+            // scale and reset quantiies of ingrediants
+            // should not store data, only use memory (no .txt) arrays 
+        }
+    }
+}
+
