c9806f58a66e104f2d1be315791b7fc88439ef47
dean <deangibson03@gmail.com>
2024-02-29 11:40:58 +0200

    added recip class

--- POE/Program.cs
+++ POE/Program.cs
@@ -12,7 +12,31 @@
             // must be able to clear the recipe, and add a new one
             // scale and reset quantiies of ingrediants
             // should not store data, only use memory (no .txt) arrays 
+
+            //recipe recipe new Recipe(); // create object to reference static 
+            Recipe recipe = new Recipe();
+            recipe.getRecipe();
+        }
+
+    }
+    class Recipe
+    {
+        public Recipe()
+        {
+            // contructor
+        }
+
+        public void getRecipe()
+        {
+            Console.WriteLine("What is the name of your recipe?");
+            String recipeName = Console.ReadLine();
+
+            Console.WriteLine("List the ingridients seperated by a ,");
+            String allIngriedints = Console.ReadLine();
+
         }
     }
 }
+      
+
 
