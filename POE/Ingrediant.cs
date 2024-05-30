9a28a833fa06b2412bcffbcd68ecd2829f36bde0
Dean Gibson <deangibson03@gmail.com>
2024-05-30 17:00:53 +0200

    Moved ingrediant class into its seperate file

---
+++ POE/Ingrediant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_PART1
+{
+    public class Ingredient // handles ingrediants, steps and scale
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string UnitOfMeasure { get; set; }
+        public double Calories { get; set; }
+        public string FoodGroup { get; set; }
+
+        public Ingredient(string name, double quantity, string unitOfMeasure, double calories, string foodGroup)
+        {
+            Name = name;
+            Quantity = quantity;
+            UnitOfMeasure = unitOfMeasure;
+            Calories = calories;
+            FoodGroup = foodGroup;
+        }
+
+        public Ingredient Clone()
+        {
+            return new Ingredient(Name, Quantity, UnitOfMeasure, Calories, FoodGroup);
+        }
+    }
+}
--- POE/Recipe.cs
+++ POE/Recipe.cs
@@ -1,28 +1,5 @@
 namespace POE_PART1
 {
-    public class Ingredient // handles ingrediants, steps and scale
-    {
-        public string Name { get; set; }
-        public double Quantity { get; set; }
-        public string UnitOfMeasure { get; set; }
-        public double Calories { get; set; }
-        public string FoodGroup { get; set; }
-
-        public Ingredient(string name, double quantity, string unitOfMeasure, double calories, string foodGroup)
-        {
-            Name = name;
-            Quantity = quantity;
-            UnitOfMeasure = unitOfMeasure;
-            Calories = calories;
-            FoodGroup = foodGroup;
-        }
-
-        public Ingredient Clone()
-        {
-            return new Ingredient(Name, Quantity, UnitOfMeasure, Calories, FoodGroup);
-        }
-    }
-
     public class Recipe
     {
         public string Name { get; set; }
